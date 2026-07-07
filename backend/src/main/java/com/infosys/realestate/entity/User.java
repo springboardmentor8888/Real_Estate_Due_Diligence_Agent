b524390b14710d2ec12efbbeb3ca3b1f32325b41
Bharani-T21 <bharanit05@gmail.com>
2026-07-07 19:38:22 +0530

    Initial backend setup with package structure and User entity

---
+++ backend/src/main/java/com/infosys/realestate/entity/User.java
@@ -0,0 +1,4 @@
+package com.infosys.realestate.entity;
+
+public class User {
+}
---
+++ backend/src/test/java/com/infosys/realestate/RealEstateDueDiligenceAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateDueDiligenceAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
